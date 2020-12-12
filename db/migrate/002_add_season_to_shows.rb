bbf53280e52433e97e4388b8ab0e6cf874ece80e
Alice Genkin <agenkin1@gmail.com>
2020-12-12 13:09:22 -0500

    Done.

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,10 +1,6 @@
-class AddSeason < ActiveRecord::Migration[5.2]
+class AddSeasonToShows < ActiveRecord::Migration[5.2]
   def change
-    create_table :shows do |t|
-      t.string :name
-      t.string :network
-      t.string :day
-      t.integer :rating
+    add_column :shows do |t|
       t.string :season
     end
   end
